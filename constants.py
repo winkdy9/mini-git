008bde95d069701713e73b648ecaa583182d1bd2
Анастасия <501696@niuitmo.ru>
2026-06-29 21:48:01 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+PYGIT_DIR = ".pygit"
+OBJECTS_DIR = ".pygit/objects"
+REFS_DIR = ".pygit/refs"
+HEADS_DIR = ".pygit/refs/heads"
+HEAD_FILE = ".pygit/HEAD"
+INDEX_FILE = ".pygit/index"
+
+MAIN_BRANCH = "main"
+HEAD_FILE_CONTENT = f"ref: refs/heads/{MAIN_BRANCH}\n"
